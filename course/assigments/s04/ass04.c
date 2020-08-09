2fe94a1c22404134d9ce6f88a6072d4d08600731
None <None>
2020-08-09 12:26:20 -0700

    add folders

---
+++ course/assigments/s04/ass04.c
@@ -0,0 +1,25 @@
+/*get_broadcast_address(char *ip_addr,char mask,char output_buffer);
+
+
+
+get_ip_integer_equivalent(char *ip_address);
+char *ip_address = '192.168.0.10";
+*/
+
+
+#include <stdio.h>
+
+
+int32_p get_ip_integer_equivalent(char *ip_address);
+
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
+}
+
+int32_p get_ip_integer_equivalent(char *ip_address){
+}
+}
+
+
